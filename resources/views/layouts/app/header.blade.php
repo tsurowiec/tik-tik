792b4e7647a2c72479a3f374633cdebf6321689d
Tomasz Surowiec <tsurowiec@gmail.com>
2026-05-23 08:46:48 +0200

    [] removed sidebar

--- resources/views/layouts/app/header.blade.php
+++ resources/views/layouts/app/header.blade.php
@@ -13,34 +13,7 @@
             <x-desktop-user-menu />
         </flux:header>
 
-        <!-- Mobile Menu -->
-        <flux:sidebar collapsible="mobile" sticky class="lg:hidden border-e border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
-            <flux:sidebar.header>
-                <x-app-logo :sidebar="true" href="{{ route('tasks') }}" wire:navigate />
-                <flux:sidebar.collapse class="in-data-flux-sidebar-on-desktop:not-in-data-flux-sidebar-collapsed-desktop:-mr-2" />
-            </flux:sidebar.header>
-
-            <flux:sidebar.nav>
-                <flux:sidebar.group :heading="__('Platform')">
-                    <flux:sidebar.item icon="check-circle" :href="route('tasks')" :current="request()->routeIs('tasks')" wire:navigate>
-                        {{ __('Tasks')  }}
-                    </flux:sidebar.item>
-                </flux:sidebar.group>
-            </flux:sidebar.nav>
-
-            <flux:spacer />
-
-            <flux:sidebar.nav>
-                <flux:sidebar.item icon="folder-git-2" href="https://github.com/laravel/livewire-starter-kit" target="_blank">
-                    {{ __('Repository') }}
-                </flux:sidebar.item>
-                <flux:sidebar.item icon="book-open-text" href="https://laravel.com/docs/starter-kits#livewire" target="_blank">
-                    {{ __('Documentation') }}
-                </flux:sidebar.item>
-            </flux:sidebar.nav>
-        </flux:sidebar>
-
-        <div id="container" class="flex-1 bg-indigo-50 dark:bg-mist-900">
+<div id="container" class="flex-1 bg-indigo-50 dark:bg-mist-900">
             <div class="mx-auto w-full max-w-3xl">
                 {{ $slot }}
             </div>
